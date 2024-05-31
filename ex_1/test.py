8b59047d21e4d04b6bc72d3b7eec7675365dd186
= <=>
2024-05-31 10:18:18 +0200

    change folder structure

---
+++ ex_1/test.py
@@ -0,0 +1,19 @@
+# importing function from another script
+from function import total
+
+""" running tests """
+
+# Basic operation check:
+print(total([1, 2, 3]) == 6)
+
+# Verify that the sum works with a negative and a positive number:
+print(total([1, -1]) == 0)
+
+# Verify that the sum works with two negative numbers:
+print(total([-1, -1]) == -2)
+
+# Verify that the sum works with only one element:
+print(total([1]) == 1)
+
+# Verify that the empty list returns 0:
+print(total([]) == 0)

3273ada3824b3c976e190e17312fc1639d2dad54
= <=>
2024-05-31 14:53:02 +0200

    updated with parametrization

--- practice_exercise/wallet_test_updated.py
+++ practice_exercise/wallet_test_updated.py
@@ -37,3 +37,11 @@ def wallet():
 
 def test_default_initial_amount(empty_wallet):
     assert empty_wallet.balance == 0
+
+
+@pytest.mark.parametrize("earned, spent, expected", [(30, 10, 20), (20, 2, 18)])
+def test_transactions(earned, spent, expected):
+    my_wallet = Wallet()
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected

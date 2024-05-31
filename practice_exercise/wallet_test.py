c5ba8cb41467ca541a7c5f1bd834519ce4fd4019
= <=>
2024-05-31 14:43:33 +0200

    insertinf pytest.fixture

--- practice_exercise/wallet_test.py
+++ practice_exercise/wallet_test.py
@@ -1,10 +1,12 @@
 from wallet import Wallet, InsufficientAmount
 import pytest
 
+
 def test_newly_created_wallet_with_balance_0():
 
     wallet = Wallet()
     assert wallet.balance == 0
+
 
 def test_newly_created_wallet_with_balance_100():
 
@@ -18,14 +20,30 @@ def test_newly_created_wallet_with_balance_10_then_added_90():
     wallet.add_cash(90)
     assert wallet.balance == 100
 
+
 def test_newly_created_wallet_with_balance_20_then_removed_10():
 
     wallet = Wallet(20)
     wallet.spend_cash(10)
     assert wallet.balance == 10
 
+
 def test_walles_with_InsufficientAmount():
 
     wallet = Wallet(10)
     with pytest.raises(InsufficientAmount):
         wallet.spend_cash(20)
+
+
+@pytest.fixture
+def empty_wallet():
+    return Wallet()
+
+
+@pytest.fixture
+def wallet():
+    return Wallet(19)
+
+
+def test_default_initial_amount(empty_wallet):
+    assert empty_wallet.balance == 0
